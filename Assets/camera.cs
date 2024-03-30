11d463df1de4f17ac8dd7a6cd1c6b287b9f37687
RoaringCow <ersandemircan57@gmail.com>
2024-03-30 23:10:23 +0300

    zoom

--- Assets/camera.cs
+++ Assets/camera.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class camera : MonoBehaviour
 {
-    [SerializeField] private GameObject mode;
-    [SerializeField] private object_mode mode_script;
+    private GameObject mode;
+    private object_mode mode_script;
     [SerializeField] private bool dragging = false;
     [SerializeField] private Vector3 drag_start;
+
+    private Camera cam;
 
     void Start()
     {
         mode = GameObject.Find("object_mode");
         mode_script = mode.GetComponent<object_mode>();
+        cam = this.GetComponent<Camera>();
     }
 
     void Update()
@@ -21,7 +24,6 @@ public class camera : MonoBehaviour
         {
             dragging = true;
             drag_start = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Debug.Log(drag_start);
         }
         else if (Input.GetMouseButtonUp(0))
         {
@@ -32,5 +34,15 @@ public class camera : MonoBehaviour
             Vector3 current_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             transform.Translate(new Vector3((current_pos.x - drag_start.x) * -1, (current_pos.y - drag_start.y) * -1, (current_pos.z - drag_start.z) * -1));
         }
+    }
+
+    public void ZoomIn()
+    {
+        cam.orthographicSize -= 0.5f;
+    }
+
+    public void ZoomOut()
+    {
+        cam.orthographicSize += 0.5f;
     }
 }
