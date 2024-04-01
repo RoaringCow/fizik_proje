95c55652f7b112bbb3163d1d5d1ec68989f1c233
RoaringCow <ersandemircan57@gmail.com>
2024-04-02 00:07:55 +0300

    arrow update

--- Assets/Arrows.cs
+++ Assets/Arrows.cs
@@ -6,27 +6,37 @@ using TMPro;
 public class Arrows : MonoBehaviour
 {
     public GameObject arrow_x,arrow_tip_x,arrow_tip_y, arrow_y,_object;
-    public TMP_InputField inpu_speed_x, input_speed_y;
+    TMP_InputField input_speed_x, input_speed_y;
+    public GameObject input_speed_x_object, input_speed_y_object;
     public SpriteRenderer arrowX_renderer,arrowHeadX_renderer, arrowHeadY_renderer,arrowY_renderer;
     public Rigidbody2D speed;
     void Start()
     {
-
+        input_speed_x = input_speed_x_object.GetComponent<TMP_InputField>();
+        input_speed_y = input_speed_y_object.GetComponent<TMP_InputField>();
     }
 
 
     void Update()
     {
 
-        arrow_x.transform.localScale = new Vector2(Mathf.Abs(speed.velocity.x / 5), 0.2f);
-        arrow_y.transform.localScale = new Vector2(0.2f, Mathf.Abs(speed.velocity.y / 5));
-        if (speed.velocity.x > 0)
+        if (!float.TryParse(input_speed_x.text.Replace(",", "."), out float new_speed_x))
+        {
+            new_speed_x = speed.velocity.x;
+        }
+        if (!float.TryParse(input_speed_y.text.Replace(",", "."), out float new_speed_y))
+        {
+            new_speed_y = speed.velocity.y;
+        }
+        arrow_x.transform.localScale = new Vector2(Mathf.Abs(new_speed_x / 5), 0.2f);
+        arrow_y.transform.localScale = new Vector2(0.2f, Mathf.Abs(new_speed_y / 5));
+        if (new_speed_x > 0)
         {
             arrow_tip_x.transform.localScale = new Vector2(0.4f,0.4f);
             arrow_x.transform.localPosition = new Vector2(arrow_x.transform.localScale.x / 2 + 1, 0);
             arrow_tip_x.transform.localPosition = new Vector2(arrow_x.transform.localScale.x + 1, 0);
         }
-        else if(speed.velocity.x < 0)
+        else if(new_speed_x< 0)
         {
             arrow_tip_x.transform.localScale = new Vector2(0.4f, -0.4f);
             arrow_x.transform.localPosition = new Vector2((arrow_x.transform.localScale.x * -1) / 2 - 1, 0);
@@ -38,13 +48,13 @@ public class Arrows : MonoBehaviour
         }
 
 
-        if (speed.velocity.y > 0)
+        if (new_speed_y > 0)
         {
             arrow_tip_y.transform.localScale = new Vector2(0.4f, 0.4f);
             arrow_y.transform.localPosition = new Vector2(0, arrow_y.transform.localScale.y / 2 + 1);
             arrow_tip_y.transform.localPosition = new Vector2(0, arrow_y.transform.localScale.y + 1);
         }
-        else if (speed.velocity.y < 0)
+        else if (new_speed_y < 0)
         {
             arrow_tip_y.transform.localScale = new Vector2(0.4f,- 0.4f);
             arrow_y.transform.localPosition = new Vector2(0,(arrow_y.transform.localScale.y * -1) / 2 - 1);
