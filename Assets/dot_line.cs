9745412740718f52d3335a0cd15c536fca60e7d3
RoaringCow <ersandemircan57@gmail.com>
2024-04-10 18:36:57 +0300

    added distance text

--- Assets/dot_line.cs
+++ Assets/dot_line.cs
@@ -9,17 +9,18 @@ public class dot_line : MonoBehaviour
     GameObject ball_object;
     public GameObject length_text_object;
     TMP_Text length_text;
+    Camera cam;
     void Start()
     {
+        cam = GameObject.Find("Main Camera").GetComponent<Camera>();
         length_text = length_text_object.GetComponent<TMP_Text>();
         ball_object = GameObject.Find("Object");
         lr = GetComponent<LineRenderer>();
         float x = ball_object.transform.position.x;
-        Debug.Log("sex");
-        length_text_object.transform.position = new Vector3(x , (transform.position.y - 15) / 2, 0);
-        length_text.text = transform.position.y + 15 + " birim";
+        length_text_object.transform.position = cam.WorldToScreenPoint(new Vector3(x , (transform.position.y - 14.455f) / 2, 0));
+        length_text.text = transform.position.y + 14.455f + " birim";
         lr.SetPosition(0, new Vector3(x, transform.position.y, 0));
-        lr.SetPosition(1, new Vector3(x, -15, 0));
+        lr.SetPosition(1, new Vector3(x, -14.455f, 0));
     }
 
 }
